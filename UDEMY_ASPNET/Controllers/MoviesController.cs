47f9200db36d473e2dc92ba41f88a49a694b0b2b
Felip3FL <felip3.fl@gmail.com>
2019-03-07 07:08:00 -0300

    2.10 Action Results

--- UDEMY_ASPNET/Controllers/MoviesController.cs
+++ UDEMY_ASPNET/Controllers/MoviesController.cs
@@ -9,12 +9,20 @@ namespace UDEMY_ASPNET.Controllers
 {
     public class MoviesController : Controller
     {
+
+        //https://blog.eduonix.com/web-programming-tutorials/learn-different-types-of-action-results-in-mvc/
+        //https://docs.microsoft.com/pt-br/dotnet/api/system.web.mvc.actionresult?view=aspnet-mvc-5.2#derived
+
         // GET: Movies
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
+            //return Content("Olá Mundo!!!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
